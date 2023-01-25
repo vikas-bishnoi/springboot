fcfc5a045c8737412ee51e1fb7d97c650a26f46b
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-26 01:22:41 +0530

    Create a POJO using Grade class and added data to model

---
+++ grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -0,0 +1,37 @@
+package com.ltp.gradesubmission;
+
+public class Grade {
+    private String name;
+    private String subject;
+    private String score;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public Grade(String name, String subject, String score) {
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
