be48bf9779b0fb8e4a105190baf3052f76272c4e
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-27 03:31:15 +0530

    Add update button and redirect to home page when clicked on it

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,15 +19,30 @@ public class GradeController {
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
+
     @GetMapping("/")
-    public String getForm(Model model) {
-        model.addAttribute(new Grade());
+    public String getForm(Model model, @RequestParam(required = false) String name) {
+        int index = getGradeIndex(name);
+        Grade grade = new Grade();
+        if(index != -1)
+            grade = studentGrades.get(index);
+
+        System.out.println(index);
+        model.addAttribute("grade", grade);
         return "form";
     }
 
-    @PostMapping("/handleSubmit")
+    @PostMapping("/handle-submit")
     public String submitGrade(Grade grade) {
         studentGrades.add(grade);
         return "redirect:/grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for(int i=0; i<studentGrades.size(); i++) {
+            if(studentGrades.get(i).getName().equals(name))
+                return i;
+        }
+        return -1;
     }
 }
