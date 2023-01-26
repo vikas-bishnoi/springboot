2f21879292399b2080da9f2cc38611d7d04bdf0d
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-26 19:16:49 +0530

    Thymeleaf for loops

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class GradeController {
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "C-"),
+        new Grade("Ron", "Charms", "C"),
+        new Grade("Hermoine", "Potions", "A")
+    );
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
