33b20ee746534a78f9836e2b4e3fd4fd4571df4f
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-29 04:25:08 +0530

    Test successful get request to "/" path

--- grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -1,13 +1,35 @@
 package com.ltp.gradesubmission;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradeSubmissionApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
 	}
 
+	@Test
+	public void testShowGradeForm() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/?id=1234");
+
+		mockMvc.perform(request)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(view().name("form"))
+				.andExpect(model().attributeExists("grade"));
+	}
 }
