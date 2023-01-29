bafcb9cc67e7776b6d7f011196f95de71e956b71
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-30 01:44:24 +0530

    Make RESTFUL get api request

--- contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
+++ contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
@@ -1,6 +1,7 @@
 package com.ltp.contacts.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,11 @@ import com.ltp.contacts.pojo.Contact;
 @Repository
 public class ContactRepository {
     
-    private List<Contact> contacts = new ArrayList<>();
+    private List<Contact> contacts = Arrays.asList(
+        new Contact("1", "Jon", "+91998877"),
+        new Contact("2", "Tyrion", "+91654321"),
+        new Contact("3", "Hound", "+91123456")
+    );
 
     public List<Contact> getContacts() {
         return contacts;
--- contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
 @Service
@@ -13,7 +14,10 @@ public class ContactServiceImpl implements ContactService {
     @Autowired
     private ContactRepository contactRepository;
     
-
+    @Override
+    public Contact getContactById(String id) {
+        return contactRepository.getContact(findIndexById(id));    
+    }
 
 
     private int findIndexById(String id) {
