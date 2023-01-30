7b209bfe67f766b367d47e5b2b926e7334e0548e
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-31 05:14:36 +0530

    Create a grade table in database using @Entity annotation

--- grade-submission-sql/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ grade-submission-sql/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -1,13 +1,34 @@
 package com.ltp.gradesubmission;
 
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class GradeSubmissionApplication {
+public class GradeSubmissionApplication implements CommandLineRunner {
+	@Autowired
+	StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Student[] students = new Student[] {
+				new Student(1L, "Harry", LocalDate.parse("1980-07-31")),
+				new Student(2L, "Ron", LocalDate.parse("1980-03-01")),
+				new Student(3L, "Hermoine", LocalDate.parse("1979-09-19")),
+				new Student(4L, "Neville", LocalDate.parse("1980-07-30"))
+		};
+
+		for (Student student : students)
+			studentRepository.save(student);
 	}
 
 }
--- grade-submission-sql/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ grade-submission-sql/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -1,14 +1,22 @@
 package com.ltp.gradesubmission.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name = "grade")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Grade {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+    @Column(name="score", nullable = false)
     private String score;
 
 }
